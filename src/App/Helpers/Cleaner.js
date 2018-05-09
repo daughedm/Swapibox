b9d1125c7a4043f6e28de640967616264059b8b2
David Daugherty <david.m.daugherty@gmail.com>
2018-05-09 16:22:39 -0600

    Create preliminary cleaner functions

--- src/App/Helpers/Cleaner.js
+++ src/App/Helpers/Cleaner.js
@@ -1,13 +1,25 @@
 const crawlCleaner = (rawData) => {
   let randNum = Math.floor(Math.random() * 1) + 1;
-  let newcrawlData = rawData.results.map(film => {
-    return { title: film.title, episode_id: film.episode_id, opening_crawl: film.opening_crawl }
+  let cleanCrawlData = rawData.results.map(film => {
+    return { 
+      title: film.title, 
+      episode_id: film.episode_id, 
+      opening_crawl: film.opening_crawl 
+    }
   })
-  return newcrawlData[randNum];
+  return cleanCrawlData[randNum];
 }
 
 const peopleCleaner = (rawData) => {
-
+  const cleanPeopleData = rawData.results.map(person => {
+    return {
+      name: person.name,
+      homeworld: person.homeworld,
+      species: person.species,
+      population: person.population
+    }
+  })
+  return cleanPeopleData;
 }
 
 const planetsCleaner = (rawData) => {
@@ -15,7 +27,15 @@ const planetsCleaner = (rawData) => {
 }
 
 const vehiclesCleaner = (rawData) => {
-
+  const cleanVehiclesData = rawData.results.map(vehicle => {
+    return {
+      name: vehicle.name,
+      model: vehicle.model,
+      vehicle_class: vehicle.vehicle_class,
+      passengers: vehicle.passengers
+    }
+  })
+  return cleanVehiclesData;
 }
 
 
