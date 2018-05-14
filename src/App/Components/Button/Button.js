1286b10ba6514490a13f2ea7ddd14a1f75bd9257
David Daugherty <david.m.daugherty@gmail.com>
2018-05-13 21:43:30 -0600

    Add locking hoverstates to butons when clicked and changed syling of hearts

--- src/App/Components/Button/Button.js
+++ src/App/Components/Button/Button.js
@@ -5,59 +5,59 @@ import Falcon from '../../../Assets/Images/falcon.svg';
 import '../Button/Button.css';
 import PropTypes from 'prop-types';
 
-const Button = ({ handleClick, favorites }) => {
+const Button = ({ handleClick, favorites, currentCategory }) => {
 
   return (
     <div className="buttons-wrapper" >
       <div 
         id="people-button"
-        className="button-container" 
+        className="button-container"
         onClick={() => handleClick('people')} 
         type="button" 
       >
-        <div className="circle">
+        <div className={currentCategory !== 'people' ? "circle" : "border"}>
           <img 
             src={Vader} 
             alt="people" 
             className="icon"/>
         </div>
-        <h2 className="btnTitle">PEOPLE</h2>
+        <h2 className={currentCategory === 'people' ? "btnTitle yellow" : "btnTitle"}>PEOPLE</h2>
       </div>
       <div 
         id="planet-button"
         className="button-container" 
         onClick={() => handleClick('planets')} 
         type="button">
-        <div className="circle">
+        <div className={currentCategory !== 'planets' ? "circle" : "border"}>
           <img 
             src={DeathStar} 
             alt="planets" 
             className="icon"/>
         </div>
-        <h2 className="btnTitle">PLANETS</h2>
+        <h2 className={currentCategory === 'planets' ? "btnTitle yellow" : "btnTitle"}>PLANETS</h2>
       </div>
       <div 
         id="vehicle-button"
         className="button-container" 
         onClick={() => handleClick('vehicles')} 
         type="button">
-        <div className="circle" >
+        <div className={currentCategory !== 'vehicles' ? "circle" : "border"} >
           <img 
             src={Falcon} 
             alt="vehicles" 
             className="icon"/>
         </div>
-        <h2 className="btnTitle">VEHICLES</h2>
+        <h2 className={currentCategory === 'vehicles' ? "btnTitle yellow" : "btnTitle"}>VEHICLES</h2>
       </div>
       <div 
         id="favorite-button"
         className="button-container divider"
         onClick={() => handleClick('favorites')}
       >
-        <div className="circle filled">
+        <div className={currentCategory !== 'favorites' ? "circle filled" : "border filled"}>
           <h3 className="favorite-number">{favorites.length ? favorites.length : 0}</h3>
         </div>
-        <h2 className="btnTitle">FAVORITES</h2>
+        <h2 className={currentCategory === 'favorites' ? "btnTitle yellow" : "btnTitle"}>FAVORITES</h2>
       </div>
     </div>
   );
